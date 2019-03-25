eff2e8e7b47fc689eaacdc13fd30be4c3a909521
Kirill Kovzel <Kirill.Kovzel@gmail.com>
2019-03-25 17:21:49 +0300

    feature/routing,
    * add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [{
+    path: 'login',
+    component: LoginComponent
+  }, {
+    path: 'home',
+    component: HomeComponent
+  }, {
+    path: '**',
+    redirectTo: 'home',
+    // component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
